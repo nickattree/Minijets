eec39b0a1a7d96b4d800860719e0d56bb9e501d9
Nick <nickattree@gmail.com>
2016-02-17 14:25:16 +0000

    Changed png to pdf output and added corotating longitudes

--- mj_readdata.py
+++ mj_readdata.py
@@ -5,13 +5,28 @@ from datetime import datetime
 
 data = pandas.read_csv('Minijets.csv', na_values="-")
 
+# Extract date and time information from string
 t =pandas.to_datetime(data.Date, format='%Y-%jT%H:%M:%S.%f')
 
-epoch = 2454102.00
+# Parameters of the system - hardwired in
+# Epoch for computing corotating longitudes. Somewhat arbitrary but must be consistent
+epoch = pandas.to_datetime('2007-001T12:00:00.000', format='%Y-%jT%H:%M:%S.%f')
+# Mean motion of the F ring in deg/day
 mmf = 581.96
 
-plt.plot(data.Base_Longitude, data.Time, 'ro')
+# Derived minijet properties
+# Elapsed seconds between measurment and epoch
+secs=(epoch-t).dt.total_seconds()
+#Corotating longitude
+nl_base = data.Base_Longitude + secs * mmf/24.0/60.0/60.0%360
+nl_tip = data.Tip_Longitude + secs * mmf/24.0/60.0/60.0%360
+nl_base[nl_base > 360.0] = nl_base-360
+nl_tip[nl_tip > 360.0] = nl_tip-360
+
+# Basic plot to show everything worked
+plt.plot(nl_base, t, 'ro')
 plt.xlim(0, 360)
-plt.xlabel(r'$\lambda$')
+plt.xlabel(r'$\lambda_n$')
+plt.ylabel('Date')
 plt.xticks(np.linspace(0, 360, 6, endpoint=True))
-plt.savefig('Minijets.png')
+plt.savefig('Minijets.pdf')
