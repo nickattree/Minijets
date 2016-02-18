95e3fd60cc70b3a8b4fbdb5e3b23a6bd4f179d35
Nick <nickattree@gmail.com>
2016-02-18 15:29:20 +0000

    Added mj_timetrend.py and series.csv and changed plot names

--- mj_readdata.py
+++ mj_readdata.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
-from datetime import datetime
 
 data = pandas.read_csv('Minijets.csv', na_values="-")
 
@@ -11,22 +10,36 @@ t =pandas.to_datetime(data.Date, format='%Y-%jT%H:%M:%S.%f')
 # Parameters of the system - hardwired in
 # Epoch for computing corotating longitudes. Somewhat arbitrary but must be consistent
 epoch = pandas.to_datetime('2007-001T12:00:00.000', format='%Y-%jT%H:%M:%S.%f')
-# Mean motion of the F ring in deg/day
+# Mean motion and semi-major axis of the F ring in deg/day and km
 mmf = 581.96
+af = 140221.0
 
 # Derived minijet properties
 # Elapsed seconds between measurment and epoch
 secs=(epoch-t).dt.total_seconds()
-#Corotating longitude
+# Corotating longitude
 nl_base = data.Base_Longitude + secs * mmf/24.0/60.0/60.0%360
 nl_tip = data.Tip_Longitude + secs * mmf/24.0/60.0/60.0%360
 nl_base[nl_base > 360.0] = nl_base-360
 nl_tip[nl_tip > 360.0] = nl_tip-360
+# Lengths and angles
+r = data.Tip_Radius - data.Base_Radius
+l = (data.Tip_Longitude - data.Base_Longitude) * af * np.pi / 180.
+length = np.sqrt(r**2 + l**2)
+ratio = r/l
+angle = np.arctan(ratio) * 180.0 / np.pi
 
-# Basic plot to show everything worked
-plt.plot(nl_base, t, 'ro')
-plt.xlim(0, 360)
-plt.xlabel(r'$\lambda_n$')
-plt.ylabel('Date')
-plt.xticks(np.linspace(0, 360, 6, endpoint=True))
-plt.savefig('Minijets.pdf')
+# Basic plots to show everything worked
+
+# Histogram of the radial sizes of minijets
+plt.figure(figsize=(8, 6), dpi=80)
+bins = np.arange(-200, 201, 5)
+hist = np.histogram(r[data.Class == 'Classic'], bins=bins)[0]
+bins = 0.5*(bins[1:] + bins[:-1])
+plt.bar(bins, hist, width = 5, fill = False)
+plt.xlim(-200, 200)
+plt.xlabel('r (km)')
+plt.ylabel('Number')
+plt.savefig('plot_r.pdf')
+
+# Histogram of the gradients of minijets
---
+++ mj_timetrend.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 11:13:28 2016
+
+@author: noa
+"""
+
+import datetime
+
+seriesdata = pandas.read_csv('Series.csv', na_values="-")
+seriesdata.Start_Time=seriesdata.Start_Time.astype(str)
+seriesdata.Stop_Time=seriesdata.Stop_Time.astype(str)
+
+# Extract datetimes from strangely formatted strings
+# Fracdays is needed to deal with fractional amounts of days. TO BE CLEANED UP
+fracdays = seriesdata.Start_Time.str[7:].astype(float)
+fracdays = pandas.to_timedelta(fracdays, 'D')
+fracdays2 = seriesdata.Stop_Time.str[7:].astype(float)
+fracdays2 = pandas.to_timedelta(fracdays2, 'D')
+# slices up string and date converts each bit
+start = pandas.to_datetime(seriesdata.Start_Time.str[0:4] + '-' + seriesdata.Start_Time.str[4:7], format = '%Y-%j')
+start = start + fracdays
+stop = pandas.to_datetime(seriesdata.Stop_Time.str[0:4] + '-' + seriesdata.Stop_Time.str[4:7], format = '%Y-%j')
+stop = stop + fracdays2
+
+# Location of all the features in space and time
+# Need to fix round the end erros where Startlong<360<Stoplong
+plt.figure(figsize=(8, 6), dpi=80)
+plt.plot([seriesdata.Start_Longitude,seriesdata.Stop_Longitude],[start,stop], color = 'Black')
+plt.plot(nl_base, t, 'ro')
+plt.xlim(0, 360)
+plt.xlabel(r'$\lambda_n$ ($^{\circ}$)')
+plt.ylabel('Date')
+plt.xticks(np.linspace(0, 360, 5, endpoint=True))
+plt.savefig('plot_coverage.pdf')
